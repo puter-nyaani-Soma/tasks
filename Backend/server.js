aabbbd807d9342334f1642acd478981f12f08937
Somaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa <sivabala2053@gmail.com>
2023-06-13 02:38:29 +0530

    first commit

---
+++ Backend/server.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+const express= require('express');
+const app = express();
+
+app.get('/',(req,res)=>{
+    res.send("hi")
+})
+app.listen(process.env.PORT,() => {
+    console.log(`listening on port ${process.env.PORT}`);
+})
