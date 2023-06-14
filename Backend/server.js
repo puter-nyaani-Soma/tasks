24702d62bbf8902151ac2a05ec2538e36d4bd1d6
Somaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa <sivabala2053@gmail.com>
2023-06-14 08:24:34 +0530

    updated-git-ignore

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,35 @@
 require('dotenv').config();
 const express= require('express');
+const mongoose = require('mongoose');
 const app = express();
 
+//import routes
+const taskRoutes = require('./routes/tasks');
+
+//middle wares
+app.use((req, res, next) => {
+    console.log(req.path,req.method)
+    next();
+})
+app.use(express.json())
+
+//routes
+
+app.use('/api/tasks',taskRoutes)
 app.get('/',(req,res)=>{
     res.send("hi")
 })
-app.listen(process.env.PORT,() => {
-    console.log(`listening on port ${process.env.PORT}`);
+
+//connenction
+
+mongoose.connect(process.env.MONGO_URI)
+.then(
+    //listen 
+    app.listen(process.env.PORT,() => {
+        console.log(`listening on port ${process.env.PORT}`);
+    })
+)
+.catch((error)=>{
+    console.log(`error: ${error}`)
 })
+//mongoose.connect('mongodb://127.0.0.1:27017/testdb', { useNewUrlParser: true, useUnifiedTopology: true })
